f22faaa4587f020390450dbd7c70bb80f82544ec
Ronal <32490001+Ronal-H@users.noreply.github.com>
2022-04-23 13:54:51 +0800

    add: User-Agent

--- news_36r.py
+++ news_36r.py
@@ -2,11 +2,14 @@ import requests
 from lxml import etree
 import time
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4146.4 Safari/537.36'
+}
 
 def req_for_36kr_news():
     # 36氪新闻资讯每日快讯
     # 查询8点1氪用户文章列表
-    resp = requests.get("https://36kr.com/user/5652071")
+    resp = requests.get("https://36kr.com/user/5652071", headers=headers)
     parse_html = etree.HTML(resp.content)
     # 获取第一篇文档dom
     parse_html
@@ -14,7 +17,7 @@ def req_for_36kr_news():
         '//*[@class="author-detail-flow-list "]/div[1]/div/div/div[2]/div[1]/a'
     )
     # 打开第一篇文章
-    today_news_resp = requests.get("https://36kr.com" + first_new[0].get("href"))
+    today_news_resp = requests.get("https://36kr.com" + first_new[0].get("href"), headers=headers)
     today_news_html = etree.HTML(today_news_resp.content)
     # 提取文件内容
     today_news_html_list = today_news_html.xpath(
